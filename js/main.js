dd7be374f968ec410d131a37be4207913a4746f6
Aarón <aaron.rc24@gmail.com>
2021-08-11 13:42:12 -0500

    Carga inicial de archivos

---
+++ js/main.js
@@ -0,0 +1,27 @@
+window.onload = () => {
+    'use strict';
+  
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+               .register('./sw.js');
+    }
+  
+    var btn = document.getElementById("copy-address"); 
+    btn.addEventListener("click", (evt) => {
+        var input = document.querySelector("#hidden-ip-address");
+        input.select();
+        input.setSelectionRange(0, 99999)
+        document.execCommand("copy");
+        console.log(input.value);
+    });
+
+    fetch("https://publicip.pielusa.fun/")
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      document.getElementById("ip-address").innerHTML = data.ip;
+      document.querySelector("#hidden-ip-address").value = data.ip;
+    });
+  }
+  
+  
